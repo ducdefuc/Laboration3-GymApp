fd048eb192185d5df65ab17f490befe542c25599
DucAnh Pham <ducanh.p@outlook.com>
2023-10-19 15:10:34 +0200

    Added functionallity to show exercises in a workout, for now testing by harcoding exercise values.

--- src/controllers/createWorkoutController.js
+++ src/controllers/createWorkoutController.js
@@ -11,6 +11,11 @@ export default class createWorkoutController {
   createWorkoutPost(req, res, next) {
     const workoutName = this.#getWorkoutName(req);
     this.gymLibrary.createWorkout(workoutName);
+    const pushups = this.gymLibrary.createExercise("PushUps", "press yourself away from the floor");
+    this.gymLibrary.addExerciseToWorkout(workoutName, pushups);
+    pushups.addWarmupSet(10, 10);
+    pushups.addWorkingSet(20, 10);
+    console.log(this.gymLibrary.getWorkout(workoutName));
     this.#redirectToHomePage(res);
   }
 
