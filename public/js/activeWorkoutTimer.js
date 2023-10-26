a8e0754fcce6737d2bd1ec015aee0ef12d9f34ea
DucAnh Pham <ducanh.p@outlook.com>
2023-10-26 22:27:46 +0200

    refactored activeWorkoutTimer.js

--- public/js/activeWorkoutTimer.js
+++ public/js/activeWorkoutTimer.js
@@ -1,19 +1,43 @@
 class ActiveWorkoutTimer {
+
+  #time;
+  #hoursSpanElement;
+  #minutesSpanElement;
+  #secondsSpanElement;
+  #restTimerButton;
+
   constructor() {
-    this.hours = 0;
-    this.minutes = 0;
-    this.seconds = 0;
-    this.hoursSpan = document.getElementById('hours');
-    this.minutesSpan = document.getElementById('minutes');
-    this.secondsSpan = document.getElementById('seconds');
-    this.timerControl = document.getElementById('timerControl');
+    this.#time = { hours: 0, minutes: 0, seconds: 0 };
+    this.#hoursSpanElement = document.getElementById('hours');
+    this.#minutesSpanElement = document.getElementById('minutes');
+    this.#secondsSpanElement = document.getElementById('seconds');
+    this.#restTimerButton = document.getElementById('restTimerButton');
     this.#startTimer();
   }
 
+  #startTimer() {
+    setInterval(() => {
+      this.#updateTimeOnOverlap();
+      this.#updateDisplay();
+    }, 1000);
+  }
+
+  #updateTimeOnOverlap() {
+    this.#time.seconds++;
+    if (this.#time.seconds === 60) {
+      this.#time.seconds = 0;
+      this.#time.minutes++;
+      if (this.#time.minutes === 60) {
+        this.#time.minutes = 0;
+        this.#time.hours++;
+      }
+    }
+  }
+
   #updateDisplay() {
-    this.hoursSpan.textContent = this.#formatTime(this.hours);
-    this.minutesSpan.textContent = this.#formatTime(this.minutes);
-    this.secondsSpan.textContent = this.#formatTime(this.seconds);
+    this.#hoursSpanElement.textContent = this.#formatTime(this.#time.hours);
+    this.#minutesSpanElement.textContent = this.#formatTime(this.#time.minutes);
+    this.#secondsSpanElement.textContent = this.#formatTime(this.#time.seconds);
   }
 
   #formatTime(number) {
@@ -21,21 +45,6 @@ class ActiveWorkoutTimer {
       return '0' + number;
     }
     return number.toString();
-  }
-
-  #startTimer() {
-    setInterval(() => {
-      this.seconds++;
-      if (this.seconds === 60) {
-        this.seconds = 0;
-        this.minutes++;
-        if (this.minutes === 60) {
-          this.minutes = 0;
-          this.hours++;
-        }
-      }
-      this.#updateDisplay();
-    }, 1000);
   }
 }
 
